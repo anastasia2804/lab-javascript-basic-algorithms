afd5446c764351ccdea090d164492813de36ac85
Anastasia Orlova <anastasia2804@gmail.com>
2022-08-17 16:40:53 -0400

    3rd iteration 2/3 completed

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@
 let hacker1
 let hacker2
 
-hacker1 = "Anastasia"
-hacker2 = "Adam"
+hacker1 = "anastasia"
+hacker2 = "adam"
 
 console.log(`The driver's name is ${hacker1}`)
 console.log(`The navigator's name is ${hacker2}`)
@@ -20,9 +20,24 @@ if(hacker1.length > hacker2.length) {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
 
+
 // Iteration 3: Loops
+let expectedOutput = ""
 
 for (let i=0; i<hacker1.length; i++){
-    
+    // expectedOutput = expectedOutput + hacker1[i].toUpperCase() + " "
+    if (i + 1 !== hacker1.length){
+        expectedOutput = expectedOutput + hacker1[i].toUpperCase() + " "
+    } else {
+        expectedOutput += hacker1[i].toUpperCase()
+    }
 }
-console.log(hacker1[i].toUpperCase)
+console.log(expectedOutput)
+console.log(expectedOutput.length)
+
+let reverseName = ""
+
+for (let i = hacker2.length; i>0; i--){
+    reverseName += hacker2[i-1]
+}
+console.log(reverseName)
