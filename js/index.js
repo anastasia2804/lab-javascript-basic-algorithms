c4601f5a0e1a24348ca92dc922c2021c7082f88c
Anastasia Orlova <anastasia2804@gmail.com>
2022-08-17 22:44:40 -0400

    Iterations 3/3 completed

--- js/index.js
+++ js/index.js
@@ -22,6 +22,9 @@ if(hacker1.length > hacker2.length) {
 
 
 // Iteration 3: Loops
+
+//3.1 Print all the characters of the driver's name, separated by a space and in capitals 
+
 let expectedOutput = ""
 
 for (let i=0; i<hacker1.length; i++){
@@ -33,7 +36,9 @@ for (let i=0; i<hacker1.length; i++){
     }
 }
 console.log(expectedOutput)
-console.log(expectedOutput.length)
+// console.log(expectedOutput.length)
+
+//3.2 Print all the characters of the navigator's name, in reverse order
 
 let reverseName = ""
 
@@ -41,3 +46,48 @@ for (let i = hacker2.length; i>0; i--){
     reverseName += hacker2[i-1]
 }
 console.log(reverseName)
+
+//3.3  Lexicographic order 
+
+let driver="yan"
+let navigator="greg"
+
+if (driver.localeCompare(navigator) === -1){
+    console.log("The driver's name goes first.")
+} else if (driver.localeCompare(navigator) === 1){
+    console.log("Yo, the navigator goes first definitely.")
+} else if (driver.localeCompare(navigator) === 0){
+    console.log("What?! You both have the same name?")
+}
+
+//Bonus 1
+
+let loremIpsum=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel lacus facilisis, tempor ante ac, molestie augue. Proin vitae eros vitae sem ultrices pretium. Phasellus dui dui, fermentum tempor fringilla ut, tincidunt tristique magna. Nullam turpis augue, feugiat eu dapibus ut, rutrum eget felis. Aliquam ligula augue, ullamcorper vel neque non, fringilla finibus dui. Fusce lacinia condimentum ante, sit amet volutpat velit fermentum non. In hac habitasse platea dictumst. Integer interdum pretium ligula, vel sagittis nisi placerat a. Sed lacinia malesuada ex, in condimentum libero posuere sed. In non felis vel enim vehicula lobortis nec quis ante.
+Duis blandit maximus massa. Quisque enim erat, tincidunt sed aliquet quis, euismod ac urna. Fusce tempus nulla ac justo ullamcorper congue. Donec in purus pharetra felis semper fermentum. Vivamus et ante non lorem finibus bibendum id id magna. Vivamus a purus eleifend, faucibus ante at, molestie risus. Maecenas ac libero faucibus, aliquet ante ac, rhoncus dui. Morbi semper tellus dolor, nec egestas ex consectetur nec. Morbi pharetra magna in ligula faucibus finibus. Cras varius nulla ut dapibus tempor. Donec ullamcorper orci non pellentesque efficitur. Integer condimentum neque eu mauris maximus, id hendrerit orci cursus. Sed dignissim imperdiet justo. Ut iaculis libero justo, eget cursus mi hendrerit iaculis.
+Vestibulum semper congue venenatis. Praesent massa quam, pellentesque placerat faucibus et, pulvinar feugiat nulla. Etiam faucibus augue eget purus dapibus, ac fermentum ante euismod. Duis varius risus nec laoreet congue. In a leo at felis tempor consequat. Mauris erat est, aliquet sit amet blandit ac, suscipit a felis. Praesent eget varius arcu. Aliquam condimentum magna non metus ornare, eget consequat risus scelerisque.`
+
+console.log(loremIpsum.split(" "))
+console.log(loremIpsum.split(" ").length)
+
+//Count occurences
+
+let wordsInString = loremIpsum.split(" ")
+let word = "et"
+
+let count = 0;
+for (counter=0; counter<wordsInString.length; counter++) {
+    
+    if (wordsInString[counter] == word) {
+            count++
+            
+    }
+}
+console.log(count)
+
+//Bonus 2
+
+// let phraseToCheck = ""
+   
+
+
+
